48887b3bc7ca23c1ffe99d692ed4664c9ea37f34
ogNG4 <m.podobinski33@gmail.com>
2023-11-26 13:12:25 +0100

    JZ-55 - Configure Manrope font

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,23 +1,72 @@
-// the v2 config imports the css driver on web and react-native on native
+import {createFont, createTamagui} from 'tamagui';
+import {themes, tokens} from '@tamagui/themes';
+import {shorthands} from '@tamagui/shorthands';
 
-// for reanimated: @tamagui/config/v2-reanimated
+const manropeFont = createFont({
+  family: 'Manrope',
+  size: {
+    1: 12,
+    2: 14,
+    3: 15,
+    4: 17,
+    5: 19,
+    6: 22,
+    7: 24,
+    8: 27,
+    9: 30,
+    10: 34,
+    11: 36,
+    12: 40,
+    13: 48,
+    true: 15,
+  },
+  lineHeight: {
+    1: 17,
+    2: 22,
+    3: 25,
+    4: 28,
+    5: 31,
+    6: 35,
+    7: 39,
+    8: 43,
+    9: 48,
+    10: 53,
+  },
+  weight: {
+    4: '300',
+    5: '400',
+    6: '500',
+    7: '600',
+    8: '700',
+    9: '800',
+  },
+  letterSpacing: {
+    4: 0,
+    8: -1,
+  },
+  face: {
+    300: {normal: 'Manrope-Light'},
+    400: {normal: 'Manrope-Regular'},
+    500: {normal: 'Manrope-Medium'},
+    600: {normal: 'Manrope-SemiBold'},
+    700: {normal: 'Manrope-Bold'},
+    800: {normal: 'Manrope-ExtraBold'},
+  },
+});
 
-// for react-native only: @tamagui/config/v2-native
-
-import { config } from '@tamagui/config/v2-native'
-
-import { createTamagui } from 'tamagui'
-const tamaguiConfig = createTamagui(config)
-// this makes typescript properly type everything based on the config
-
-type Conf = typeof tamaguiConfig
+const config = createTamagui({
+  themes,
+  tokens,
+  shorthands,
+  fonts: {
+    body: manropeFont,
+    heading: manropeFont,
+  },
+});
+type Conf = typeof config;
 
 declare module 'tamagui' {
-
   interface TamaguiCustomConfig extends Conf {}
-
 }
-export default tamaguiConfig
-// depending on if you chose tamagui, @tamagui/core, or @tamagui/web
 
-// be sure the import and declare module lines both use that same name
+export default config;
